b5430434939205c106d3d9919f2dac62d341be4c
Jackson Currie <jackson@currie.page>
2022-06-30 02:59:43 +0000

    Change space field to name

--- src/spaces/dto/update-space.input.ts
+++ src/spaces/dto/update-space.input.ts
@@ -1,11 +1,11 @@
 import { CreateSpaceInput } from './create-space.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
+import { InputType, Field, PartialType } from '@nestjs/graphql';
 
 @InputType()
 export class UpdateSpaceInput extends PartialType(CreateSpaceInput) {
   @Field(() => String)
   id: string;
 
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => String, { description: 'Space name' })
+  name: string;
 }
--- src/spaces/schemas/space.schema.ts
+++ src/spaces/schemas/space.schema.ts
@@ -1,15 +1,18 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field } from '@nestjs/graphql';
 
 export type SpaceDocument = Space & Document;
 
 @Schema()
 @ObjectType()
 export class Space {
+  @Field(() => String, { description: 'Space ID' })
+  _id: string;
+
   @Prop()
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => String, { description: 'Space name' })
+  name: string;
 }
 
 export const SpaceSchema = SchemaFactory.createForClass(Space);
